ba371d0f633ea1b81b8ff08ccb5fef59dcc398ad
samh7 <85685871+samh7@users.noreply.github.com>
2024-02-10 01:46:03 +0300

    ...

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
+import { basename } from 'path';
 
 const production = !process.env.ROLLUP_WATCH;
 
